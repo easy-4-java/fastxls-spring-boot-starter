2441c30075415edcdb018b0d856025be4304286b
wandl <hiwepy@163.com>
2026-05-27 21:26:09 +0800

    chore: migrate groupId from com.github.hiwepy to io.github.hiwepy

--- src/main/java/com/github/hiwepy/fastxls/spring/boot/FastxlsJXLAutoConfiguration.java
+++ src/main/java/com/github/hiwepy/fastxls/spring/boot/FastxlsJXLAutoConfiguration.java
@@ -2,16 +2,16 @@
  * Copyright (C) 2018 Jeebiz (http://jeebiz.net).
  * All Rights Reserved. 
  */
-package com.github.hiwepy.fastxls.spring.boot;
+package io.github.hiwepy.fastxls.spring.boot;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.github.hiwepy.fastxls.jexcel.JXLWorkbookFiller;
-import com.github.hiwepy.fastxls.jexcel.JXLWorkbookMapper;
-import com.github.hiwepy.fastxls.jexcel.JXLWorkbookReader;
+import io.github.hiwepy.fastxls.jexcel.JXLWorkbookFiller;
+import io.github.hiwepy.fastxls.jexcel.JXLWorkbookMapper;
+import io.github.hiwepy.fastxls.jexcel.JXLWorkbookReader;
 
 @Configuration
 @ConditionalOnClass({JXLWorkbookFiller.class, JXLWorkbookMapper.class, JXLWorkbookReader.class})
