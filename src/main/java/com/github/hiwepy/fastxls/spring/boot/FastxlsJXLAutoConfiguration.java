18a99c6d361abb420d11b8773056907af6f599e5
wandl-6A72h <hiwepy@gmail.com>
2026-07-31 22:21:43 +0800

    refactor: 包名 io.github.hiwepy 改为 io.github.easy4j

--- src/main/java/com/github/hiwepy/fastxls/spring/boot/FastxlsJXLAutoConfiguration.java
+++ src/main/java/com/github/hiwepy/fastxls/spring/boot/FastxlsJXLAutoConfiguration.java
@@ -2,16 +2,16 @@
  * Copyright (C) 2018 Jeebiz (http://jeebiz.net).
  * All Rights Reserved. 
  */
-package io.github.hiwepy.fastxls.spring.boot;
+package io.github.easy4j.fastxls.spring.boot;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.github.hiwepy.fastxls.jexcel.JXLWorkbookFiller;
-import io.github.hiwepy.fastxls.jexcel.JXLWorkbookMapper;
-import io.github.hiwepy.fastxls.jexcel.JXLWorkbookReader;
+import io.github.easy4j.fastxls.jexcel.JXLWorkbookFiller;
+import io.github.easy4j.fastxls.jexcel.JXLWorkbookMapper;
+import io.github.easy4j.fastxls.jexcel.JXLWorkbookReader;
 
 @Configuration
 @ConditionalOnClass({JXLWorkbookFiller.class, JXLWorkbookMapper.class, JXLWorkbookReader.class})
